7019b605cef91551efe09857017d7360b881c971
SEINLET Nicolas <nicolas@seinlet.com>
2018-03-14 12:13:38 +0100

    Update inst_python3.sh
    Python 3.6.4

--- inst_python3.sh
+++ inst_python3.sh
@@ -1,6 +1,6 @@
-wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tgz
-tar xvf Python-3.6.3.tgz
-cd Python-3.6.3
+wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
+tar xvf Python-3.6.4.tgz
+cd Python-3.6.4
 ./configure --enable-optimizations
 make -j8
 sudo make altinstall
