e261b2e4269d4e0b80da48c67d4a977243706934
Nicolas Seinlet <nse@odoo.com>
2018-05-23 09:10:33 +0200

    Python 3.6.5

--- inst_python3.sh
+++ inst_python3.sh
@@ -1,6 +1,6 @@
-wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
-tar xvf Python-3.6.4.tgz
-cd Python-3.6.4
+wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tgz
+tar xvf Python-3.6.5.tgz
+cd Python-3.6.5
 ./configure --enable-optimizations
 make -j8
 sudo make altinstall
